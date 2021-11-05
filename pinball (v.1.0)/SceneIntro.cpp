930bca043a5e20a3485819c3595fc4416222d6e8
migon25 <miguel.garon@gmail.com>
2021-11-06 00:23:44 +0100

    Added scene intro

---
+++ pinball (v.1.0)/SceneIntro.cpp
@@ -0,0 +1,49 @@
+#include "SceneIntro.h"
+
+#include "Application.h"
+#include "ModuleTextures.h"
+#include "ModuleRender.h"
+#include "ModuleAudio.h"
+#include "ModuleInput.h"
+#include "FadeToBlack.h"
+
+SceneIntro::SceneIntro(Application* app, bool start_enabled) : Module(app, start_enabled)
+{
+}
+
+SceneIntro::~SceneIntro()
+{
+}
+
+// Load assets
+bool SceneIntro::Start()
+{
+	LOG("Loading background assets");
+
+
+	App->renderer->camera.x = App->renderer->camera.y = 0;
+
+	bool ret = true;
+
+	// Load the textures example: Title = App->textures->Load("");
+
+	return ret;
+}
+
+update_status SceneIntro::Update()
+{
+	if (App->input->GetKey(SDL_SCANCODE_SPACE) == KEY_DOWN)
+	{
+		App->fadeToBlack->Fade_To_Black(this, (Module*)App->gameScene, 90);
+	}
+
+	// Put images
+	App->renderer->Blit(Title, 0, 0, NULL);
+	App->renderer->Blit(bg1, 0, 0, NULL);
+	return UPDATE_CONTINUE;
+}
+
+bool SceneIntro::CleanUp()
+{
+	return true;
+}
---
+++ pinball (v.1.0)/SceneIntro.h
@@ -0,0 +1,39 @@
+#ifndef __SCENEINTRO_H__
+#define __SCENEINTRO_H__
+
+#include "Module.h"
+#include "Animation.h"
+#include "Globals.h"
+
+struct SDL_Texture;
+
+class SceneIntro : public Module
+{
+public:
+	// Constructor
+	SceneIntro(Application* app, bool start_enabled = true);
+
+	// Destructor
+	~SceneIntro();
+
+	// Called when the module is activated
+	// Loads the necessary textures for the map background
+	bool Start() override;
+
+	// Called at the middle of the application loop
+	// Updates the scene's background animations
+	update_status Update();
+
+	bool CleanUp();
+public:
+	// The scene sprite sheet loaded into an SDL_Texture
+	SDL_Texture* Title;
+	SDL_Texture* bg1;
+	SDL_Texture* bg2;
+	SDL_Texture* bg3;
+	SDL_Texture* bg4;
+	SDL_Texture* bg5;
+	SDL_Texture* bg6;
+};
+
+#endif	// __SCENEINTRO_H__
